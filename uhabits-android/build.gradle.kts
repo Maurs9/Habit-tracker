1e3049ed7142d642cb65bd8bccd53c9bc7f6b847
Maurs9 <107703059+Maurs9@users.noreply.github.com>
2026-09-16 13:31:43 +0300

    Bump version to 2.4.0 (versionCode 20400)

--- uhabits-android/build.gradle.kts
+++ uhabits-android/build.gradle.kts
@@ -44,8 +44,8 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        versionCode = 20301
-        versionName = "2.3.1"
+        versionCode = 20400
+        versionName = "2.4.0"
         minSdk = 28
         targetSdk = 36
         applicationId = "org.isoron.uhabits"
